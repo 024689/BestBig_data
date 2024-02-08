e9fd0603c9b4f0ae9f1badf78b9cf0cfa4fd45a2
warren Noumen <warrennoumen7@gmail.com>
2024-02-08 06:30:58 +0100

    commit

---
+++ AIRFLOW/extraction.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+wget -O ~/Downloads/$1.csv.gz https://static.openfoodfacts.org/data/en.openfoodfacts.org.products.csv.gz
+
+gunzip ~/Downloads/$1.csv.gz
+
+mv ~/Downloads/$1.csv ~/workspace/data/off_raw/$1.csv
+
+hdfs dfs -put ~/workspace/data/off_raw/$1.csv /user/ubuntu/off_raw/
+
